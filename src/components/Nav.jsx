8649a22b098620f4fa8c0f17c8b09530e8f9a5ad
erick patrick <github@erickpatrick.net>
2025-01-20 13:08:29 +0100

    adjusts navigation links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,12 @@
 export default function Nav() {
     return <nav className="w-full">
         <ul className="py-4 flex justify-between">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Menu</a></li>
-            <li><a href="#">Reservation</a></li>
-            <li><a href="#">Order Online</a></li>
-            <li><a href="#">Login</a></li>
+            <li><a className="font-semibold hover:underline" href="#">Home</a></li>
+            <li><a className="font-semibold hover:underline" href="#">About</a></li>
+            <li><a className="font-semibold hover:underline" href="#">Menu</a></li>
+            <li><a className="font-semibold hover:underline" href="#">Reservation</a></li>
+            <li><a className="font-semibold hover:underline" href="#">Order Online</a></li>
+            <li><a className="font-semibold hover:underline" href="#">Login</a></li>
         </ul>
     </nav>
 }
